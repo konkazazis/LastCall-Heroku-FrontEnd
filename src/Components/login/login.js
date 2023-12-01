a20f055d72f266e66173e4d0aa59b6629a3236be
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2023-12-01 11:32:52 +0200

    fixes

--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -10,7 +10,7 @@ import '../../App.css';
 import 'animate.css';
 import App from '../../App';
 
-axios.defaults.baseURL = 'http://lc-python-backend-b63f70e42fc6.herokuapp.com/'
+axios.defaults.baseURL = 'https://lc-python-backend-b63f70e42fc6.herokuapp.com/'
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
